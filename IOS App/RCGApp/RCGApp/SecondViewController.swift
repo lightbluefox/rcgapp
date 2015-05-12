238118d30beee839c1f0d6d8cb747f72de68c12c
iFoxxy <ifoxxy777@gmail.com>
2015-05-12 08:50:31 +0300

    Project created

---
+++ IOS App/RCGApp/RCGApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RCGApp
+//
+//  Created by iFoxxy on 12.05.15.
+//  Copyright (c) 2015 LightBlueFox. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
